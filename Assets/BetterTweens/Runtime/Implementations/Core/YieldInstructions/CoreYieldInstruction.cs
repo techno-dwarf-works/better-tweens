90019b68eff8df34c04d89b156f03f6294fd8170
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-04-08 01:18:32 +0200

    Base Settings

---
+++ Assets/BetterTweens/Runtime/Implementations/Core/YieldInstructions/CoreYieldInstruction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Better.Tweens.Runtime
+{
+    internal abstract class CoreYieldInstruction : CustomYieldInstruction
+    {
+        protected TweenCore Source { get; }
+
+        public CoreYieldInstruction(TweenCore source)
+        {
+            Source = source;
+        }
+    }
+
+    // Started, Activated, Playing, Rewinding, Updated, Paused, Stopped, Completed, Rewound, LoopCompleted, LoopRewound
+    // ProgressPassed(float), ElapsedLoops(int), RewoundedLoops(int)
+}
---
+++ Assets/BetterTweens/Runtime/Settings/TweensSettings.cs
@@ -0,0 +1,12 @@
+﻿using Better.Internal.Core.Runtime;
+using Better.ProjectSettings.Runtime;
+using Better.Singletons.Runtime.Attributes;
+
+namespace Better.Tweens.Runtime
+{
+    [ScriptableCreate(Path)]
+    public class TweensSettings : ScriptableSettings<TweensSettings>
+    {
+        public const string Path = PrefixConstants.BetterPrefix + "/" + nameof(Tweens);
+    }
+}
