5ca1cdd836cdd5110c600cfc9a71d70655ea0bf3
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-08-30 03:04:51 +0200

    Add Sequence extensions

---
+++ Assets/BetterTweens/Runtime/Extensions/SequenceExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Better.Tweens.Runtime.Utility;
+
+namespace Better.Tweens.Runtime
+{
+    public static class SequenceExtensions
+    {
+        public static Sequence PrependTweens(this Sequence self, IEnumerable<Tween> tweens)
+        {
+            if (!ValidationUtility.ValidateNullReference(self))
+            {
+                return self;
+            }
+
+            if (!ValidationUtility.ValidateNullReference(tweens))
+            {
+                return self;
+            }
+
+            foreach (var tween in tweens)
+            {
+                self.PrependTween(tween);
+            }
+
+            return self;
+        }
+
+        public static Sequence AppendTweens(this Sequence self, IEnumerable<Tween> tweens)
+        {
+            if (!ValidationUtility.ValidateNullReference(self))
+            {
+                return self;
+            }
+
+            if (!ValidationUtility.ValidateNullReference(tweens))
+            {
+                return self;
+            }
+
+            foreach (var tween in tweens)
+            {
+                self.AppendTween(tween);
+            }
+
+            return self;
+        }
+
+        public static Sequence JoinTweens(this Sequence self, IEnumerable<Tween> tweens)
+        {
+            if (!ValidationUtility.ValidateNullReference(self))
+            {
+                return self;
+            }
+
+            if (!ValidationUtility.ValidateNullReference(tweens))
+            {
+                return self;
+            }
+
+            foreach (var tween in tweens)
+            {
+                self.JoinTween(tween);
+            }
+
+            return self;
+        }
+    }
+}
--- Assets/BetterTweens/Runtime/Extensions/SettingsDataExtensions.cs
+++ Assets/BetterTweens/Runtime/Extensions/SettingsDataExtensions.cs
@@ -4,8 +4,6 @@ namespace Better.Tweens.Runtime
 {
     public static class SettingsDataExtensions
     {
-        // TODO: Add Tweens for all extsns IEnumerable<>
-        
         public static GlobalTimeScaleSettingsTween TweenGlobalTimeScale(this SettingsData self, float duration, float options, OptionsMode optionsMode = default)
         {
             return self.Tween<GlobalTimeScaleSettingsTween, SettingsData, float>(duration, options, optionsMode);
