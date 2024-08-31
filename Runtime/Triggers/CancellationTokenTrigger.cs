340ffc2060fe005105eb691496fb4c827a892fc3
OpOpYaDev <OpOpYaDev@users.noreply.github.com>
2024-08-31 01:51:55 +0000

    Merge pull request #4 from techno-dwarf-works/refactoring
    Version 0.0.3

---
+++ Runtime/Triggers/CancellationTokenTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Better.Tweens.Runtime.Actions;
+
+namespace Better.Tweens.Runtime.Triggers
+{
+    public class CancellationTokenTrigger : ActionTrigger
+    {
+        private readonly CancellationToken _cancellationToken;
+
+        public CancellationTokenTrigger(string id, TweenCoreAction action, CancellationToken cancellationToken)
+            : base(id, action)
+        {
+            _cancellationToken = cancellationToken;
+        }
+
+        public override bool Invoke(TweenCore tweenCore)
+        {
+            return _cancellationToken.IsCancellationRequested && base.Invoke(tweenCore);
+        }
+    }
+}
